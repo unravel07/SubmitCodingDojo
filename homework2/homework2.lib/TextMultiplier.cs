2f60f49ba5b22457ff2a7bc6cdd1984f6287dde9
unravel07 <mick_ben10@hotmail.com>
2018-11-14 15:55:09 +0700

    refactor code

--- homework2/homework2.lib/TextMultiplier.cs
+++ homework2/homework2.lib/TextMultiplier.cs
@@ -8,26 +8,28 @@ namespace homework2.lib
     {
         public string GetFormattedString(string text)
         {
-            var split = text.Split(',');
-            var multiple = split.Select(it => (int.Parse(it) * 11).ToString());
-            var array = multiple.ToArray();
+            var array = text.Split(',').Select(it => (int.Parse(it) * 11).ToString()).ToArray();
+            // var multiple = split.;
+            // var array = multiple.ToArray();
             var newText = new StringBuilder();
 
-            newText.Append("[\n");
-            for (int i = 0; i < split.Length; i++)
-            {
-                if (i == split.Length - 1)
-                {
-                    newText.Append("\t").Append(array[i]).Append("\n");
-                }
-                else
-                {
-                    newText.Append("\t").Append(array[i]).Append(",").Append("\n");
+            var gd = string.Join(",\n\t", array);
+            //newText.Append("[\n\t").Append(gd).Append("\n]");
+            newText.Append($"[\n\t{gd}\n]");
+            // for (int i = 0; i < split.Length; i++)
+            // {
+            //     if (i == split.Length - 1)
+            //     {
+            //         newText.Append("\t").Append(array[i]).Append("\n");
+            //         string.Join()
+            //         //newText.Append($"\t{array[i]}\n");
+            //     }
+            //     else
+            //     {
+            //         newText.Append("\t").Append(array[i]).Append(",").Append("\n");
 
-                }
-            }
-            newText.Append("]");
-
+            //     }
+            // }
             return newText.ToString();
         }
 
