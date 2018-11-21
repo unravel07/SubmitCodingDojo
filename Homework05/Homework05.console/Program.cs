a08671f50548429d4651237d97f377831140db5f
unravel07 <mick_ben10@hotmail.com>
2018-11-21 13:40:05 +0700

    complete

---
+++ Homework05/Homework05.console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework05.lib;
+
+namespace Homework05.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hw05 = new Homework05Lib();
+            hw05.listLED = new List<string>(new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" });
+            hw05.listNoLED = new List<string>(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" });
+            var noLed = "";
+
+            while (true)
+            {
+                var disPlayLED = hw05.DisplayLEDOnScreen(noLed);
+                Console.WriteLine(disPlayLED);
+                Console.Write("Please choose LED to turn On/Off: ");
+                noLed = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Homework05/Homework05.lib/Homework05Lib.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework05.lib
+{
+    public class Homework05Lib : IHomework05
+    {
+        public List<string> listLED { get; set; }
+        public List<string> listNoLED { get; set; }
+        public void setLED()
+        {
+            listLED = new List<string>(new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" });
+            listNoLED = new List<string>(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" });
+        }
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+            ledNo = ledNo.ToUpper();
+            var disPlayLED = new StringBuilder();
+
+            for (int i = 0; i < listNoLED.Count; i++)
+            {
+                if (ledNo == listNoLED[i])
+                {
+                    listLED[i] = (listLED[i] == "[ ]") ? "[i]" : "[ ]";
+                }
+            }
+            var LED = String.Join(" ", listLED);
+            var noLED = String.Join("   ", listNoLED);
+            var result = disPlayLED.Append(LED).AppendLine().Append(" ").Append(noLED).ToString();
+
+            return result;
+        }
+    }
+}
